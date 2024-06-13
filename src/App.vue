6c2090254237d3e8cf1a0fec895c7b9217b8e176
Hafiza Saba Asad <51789281+hafizasabaasad@users.noreply.github.com>
2024-06-13 15:09:53 +1000

    description added and some style updates

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
 <template>
-  <h1>Welcome to my App</h1>
-  <h2>Ninja Reaction Timer</h2>
+  <h1>Welcome to Single Page Application Demo</h1>
+  <h2>Title: Ninja Reaction Timer</h2>
+  <description />
   <button @click="start" :disabled="isPlaying">Play</button>
- <block  v-if="isPlaying" :delay="delay" @end="endgame" />
- <result v-if="showResults" :score="score" />
+  <block v-if="isPlaying" :delay="delay" @end="endgame" />
+  <result v-if="showResults" :score="score" />
 </template>
 
 <script>
+import description from "./components/description.vue";
 import block from "./components/block.vue";
 import result from "./components/result.vue";
-export  default{
-  name:'App',
-  components: { block , result },
-  data(){
+export default {
+  name: "App",
+  components: { block, result, description },
+  data() {
     return {
       isPlaying: false,
       dalay: null,
       score: null,
-      showResults: false
-    }
+      showResults: false,
+    };
   },
-  methods:{
-    start(){
-      this.delay = 1000 + Math.random() * 3000
-     this.isPlaying = true
-     this.showResults = false
-    
+  methods: {
+    start() {
+      this.delay = 1000 + Math.random() * 3000;
+      this.isPlaying = true;
+      this.showResults = false;
     },
-    endgame(reactiontime){
-     this.score = reactiontime
-     this.isPlaying = false
-     this.showResults = true
-    }
-  }
-  
-}
+    endgame(reactiontime) {
+      this.score = reactiontime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -46,10 +46,10 @@ export  default{
   color: #396999;
   margin-top: 60px;
 }
-button{
-  background: #64ad56;
-  color:black;
-  border:none;
+button {
+  background: #d84636;
+  color: white;
+  border: none;
   padding: 8px 16px;
   border-radius: 4px;
   font-size: 16px;
@@ -57,7 +57,7 @@ button{
   cursor: pointer;
   margin: 10px;
 }
-button[disabled]{
+button[disabled] {
   opacity: 0.2;
   cursor: not-allowed;
 }
